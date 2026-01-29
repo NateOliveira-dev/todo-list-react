b7445c628cbdaf4b3e947c516b47d7d2a4b702d8
Nathan Oliveira <nateoliveira.dev@gmail.com>
2026-01-29 15:35:51 -0300

    cria estrutura inicial da lista de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
 import { useState } from 'react'
 
 function App() {
-  return <div>ola react</div>
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text: "criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id:2,
+      text: "Ir pra academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id:3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    }
+  ])
+  return <div className='app'>
+    <h1>Lista de Tarefas</h1>
+    <div className='todo-list'>
+      {todos.map((todos) => (
+        <div className='todo'>
+          <div className='content'>
+            <p>{todos.text}</p>
+            <p className='category'>({todos.category})</p>
+          </div>
+          <div>
+            <button>Completar</button>
+            <button>X</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
 }
 
 export default App
